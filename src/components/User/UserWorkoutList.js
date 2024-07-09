68c88f3ce4d57700b8c5713fd0f5a2250e03e768
Sumnicky99 <sumnicky99@gmail.com>
2024-07-09 15:58:12 +0300

    Creating ul list

--- src/components/User/UserWorkoutList.js
+++ src/components/User/UserWorkoutList.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 // Component for displaying list of workouts
 const UserWorkoutList = ({ workouts }) => {
-    
+    return (
+        <ul>
+          
+        </ul>
+    );
 };
 
 export default UserWorkoutList;
