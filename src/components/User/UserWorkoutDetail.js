33139e9332f8d3803796fcce0d80ae6ae7e069d2
Sumnicky99 <sumnicky99@gmail.com>
2024-07-09 16:02:37 +0300

    Display workout name as a heading

--- src/components/User/UserWorkoutDetail.js
+++ src/components/User/UserWorkoutDetail.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 // Component for displaying details of a single workout
 const UserWorkoutDetails = ({ workout }) => {
-  
+    return (
+        <div>
+            {/* Display workout name as a heading */}
+           
+        </div>
+    );
 };
 
 export default UserWorkoutDetails;
