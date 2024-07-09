85c656d2fc5fa4fde10ee59eac1adaf312896760
Sumnicky99 <sumnicky99@gmail.com>
2024-07-09 16:03:13 +0300

    Display workout duration

--- src/components/User/UserWorkoutDetail.js
+++ src/components/User/UserWorkoutDetail.js
@@ -5,7 +5,9 @@ const UserWorkoutDetails = ({ workout }) => {
     return (
         <div>
             {/* Display workout name as a heading */}
-           
+            <h2>{workout.name}</h2>
+            {/* Display workout duration */}
+
         </div>
     );
 };
