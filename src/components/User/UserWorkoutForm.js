c94567d5ed984124d0c14002d3eeb2f0ab02d32b
Sumnicky99 <sumnicky99@gmail.com>
2024-07-09 15:42:31 +0300

    Updating the form with name and duration

--- src/components/User/UserWorkoutForm.js
+++ src/components/User/UserWorkoutForm.js
@@ -21,8 +21,55 @@ const UserWorkoutForm = ({ onSubmit }) => {
         body: JSON.stringify(newWorkout),
     })
 
-}
+    .then(response => response.json())
+    .then(data => {
+        onSubmit(data); // Call onSubmit prop with new workout data
+        // Clear form fields
+        setName('');
+        setDuration('');
+        setDate('');
+    })
+    // Log error if request fails
+    .catch(error => console.error('Error adding workout:', error)); 
 
+};
+
+return (
+     // Form element with submit handler
+    <form>
+        <div>
+            
+            <label>Name:</label>
+                    <input
+                        type="text"
+                        value={name}
+                        // Update name state
+                        onChange={(e) => setName(e.target.value)} />
+                    
+                    
+        </div>
+
+        <div>
+                <label>Duration:</label>
+                    <input
+                        type="text"
+                        value={duration}
+                        // Update duration state
+                        onChange={(e) => setDuration(e.target.value)} />
+
+                    
+        </div>
+        <div>
+                    <label>Duration:</label>
+                        <input
+                            type="text"
+                            value={duration}
+                            onChange={(e) => setDuration(e.target.value)} // Update duration state
+                        />
+            </div>
+    </form>
+
+);
 
 
 
