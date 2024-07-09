8d84b56e3c6a1310d6e23f5e60bf77ea3421c65b
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-07-09 16:14:03 +0300

    Added app routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import WorkoutList from "./components/WorkoutList";
+import WorkoutDetail from "./components/WorkoutDetail";
+import WorkoutForm from "./components/WorkoutForm";
+import ExerciseList from "./components/ExerciseList";
+import ExerciseForm from "./components/ExerciseForm";
+import UserWorkoutList from "./User/UserWorkoutList";
+import UserWorkoutForm from "./User/UserWorkoutForm";
+import UserWorkoutDetail from "./User/UserWorkoutDetail";
 
 const App = () => {
-   
-    return (
-        <div>
-            <h1>Workout Tracker</h1>
-          
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container mx-auto mt-4">
+          <Switch>
+            <Route exact path="/" component={WorkoutList} />
+            <Route path="/workouts/:id" component={WorkoutDetail} />
+            <Route path="/add-workout" component={WorkoutForm} />
+            <Route path="/exercises" component={ExerciseList} />
+            <Route path="/add-exercise" component={ExerciseForm} />
+            <Route path="/user-workouts" component={UserWorkoutList} />
+            <Route path="/add-user-workout" component={UserWorkoutForm} />
+            <Route path="/user-workouts/:id" component={UserWorkoutDetail} />
+          </Switch>
         </div>
-    );
+      </div>
+    </Router>
+  );
 };
 
 export default App;
