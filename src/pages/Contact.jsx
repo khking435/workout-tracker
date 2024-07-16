3e4b16159180fcfe9284fbb3496b65ccff563d5a
Gift <gzomandi07@gmail.com>
2024-07-16 12:27:36 +0300

    contact

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ContactUs = () => {
+    return (
+        <div>
+            <h2>Contact Us</h2>
+            <p>If you have any questions, feel free to reach out to us at:</p>
+            <ul>
+                <li>
+                    <a href="mailto:support@example.com">support@example.com</a>
+                </li>
+                <li>
+                    <a href="mailto:info@example.com">info@example.com</a>
+                </li>
+                <li>
+                    <a href="mailto:sales@example.com">sales@example.com</a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default ContactUs;
